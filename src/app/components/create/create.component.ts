a85d4c78aaddcbb6318b1e81d908dc3c768b9b40
edwinromero <ecro98.98@gmail.com>
2022-05-03 18:22:46 -0500

    Formulario para crear proyectos

--- src/app/components/create/create.component.ts
+++ src/app/components/create/create.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { Project} from "../../models/project";
+import { ProjectsService} from "../../sercices/projects.services";
+
 
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
-  styleUrls: ['./create.component.css']
+  styleUrls: ['./create.component.css'],
+  providers:[ProjectsService]
 })
 export class CreateComponent implements OnInit {
 
-  constructor() { }
+  public title:string;
+  public project: Project;
+
+
+  constructor(
+    private _projectService: ProjectsService
+  ) {
+    this.title = 'Crear Proejecto';
+    this.project = new Project('', '', '', '', 2022, '', '');
+  }
 
   ngOnInit(): void {
+  }
+  onSubmit(form:any){
+    console.log(this.project);
+
   }
 
 }
