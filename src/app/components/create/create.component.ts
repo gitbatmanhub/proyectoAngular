87d7d1a66867ad78f88ef4a06b69e9b29aee1aeb
edwinromero <ecro98.98@gmail.com>
2022-05-04 11:56:47 -0500

    Subir imagenes de los proyectos

--- src/app/components/create/create.component.ts
+++ src/app/components/create/create.component.ts
@@ -1,39 +1,54 @@
 import { Component, OnInit } from '@angular/core';
 import { Project} from "../../models/project";
 import { ProjectsService} from "../../sercices/projects.services";
+import { uploadService } from "../../sercices/upload.services";
+import { Global } from "../../sercices/global";
+import {global} from "@angular/compiler/src/util";
 
 
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
   styleUrls: ['./create.component.css'],
-  providers:[ProjectsService]
+  providers:[ProjectsService, uploadService]
 })
 export class CreateComponent implements OnInit {
 
   public title:string;
   public project: Project;
   public status: string;
+  public filesToUpload: Array<File>;
 
 
   constructor(
-    private _projectService: ProjectsService
+    private _projectService: ProjectsService,
+    private _uploadService: uploadService
   ) {
     this.title = 'Crear Projecto';
     this.project = new Project('', '', '', '', 2022, '', '');
     this.status = "";
+    this.filesToUpload=[];
   }
 
   ngOnInit(): void {
   }
 
   onSubmit(form:any){
-    console.log(this.project);
+    // Guardar datos
     this._projectService.saveProject(this.project).subscribe(
       response =>{
         if(response.project){
-          this.status = 'succes';
-          form.reset();
+
+
+          //Subir imágen
+          this._uploadService.makeFileRequest(Global.url+"upload-image/"+response.project._id, [], this.filesToUpload, 'image').then((result:any) =>{
+            this.status = 'succes';
+            form.reset();
+            console.log(result);
+          });
+
+
+
         }else{
           this.status = 'failed';
         }
@@ -42,6 +57,10 @@ export class CreateComponent implements OnInit {
 
       }
     )
+  }
+
+  fileChangeEvent(fileInput:any){
+    this.filesToUpload=<Array<File>>fileInput.target.files;
   }
 
 }
